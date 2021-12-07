baf8dff81706ae2e2119bf5b11ce853e533cb77c
Adam Mišurec <a_misurec@utb.cz>
2021-12-07 12:34:24 +0100

    test

---
+++ ciselne_soustavy2.py
@@ -0,0 +1,88 @@
+import sys
+
+# vycet cislic pro soustavy se zakladem 2 az po cislice se zakladem 32
+DIGITS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def main():
+	'''
+	Program prevede zadane cislo <cislo> z desitkove soustavy do ciselne soustavy se zakladem <cilova_ciselna_soustava>.
+	'''
+	cislo, cilova_ciselna_soustava = nactiVstup()
+
+	print(f'Zadane cislo: {cislo}, zaklad vybrane soustavy: {cilova_ciselna_soustava}, prevedene cislo: {preved(cislo, cilova_ciselna_soustava)}')
+	input('Stisknete ENTER pro ukonceni.')
+
+        
+def nactiVstup():
+	'''
+	Metoda nactiVstup nacte parametry <cislo> a <cilova_ciselna_soustava> budto z predanych parametru pri spusteni programu,
+	nebo je necha zadat uzivatelem programu, nebo vypise napovedu.
+	'''
+
+	# kontrola jestli jsou pri spousteni programu predavane parametry
+	if len(sys.argv) > 1:
+		args = sys.argv[1:]
+
+		# kontrola jestli bylo pozadovano vypsani napovedy
+		if args[0] == 'help' or args[0] == '-h':
+			print('Spustte budto s parametry: <cislo> <cilova_ciselna_soustava>(2..32), nebo help (-h) pro vypsani napovedy, nebo bez parametru pro rucni zadani hodnot uzivatelem.')
+			exit(0)
+		else:
+			# nacteni parametru <cislo> a <cilova_ciselna_soustava> pri spusteni programu
+			try:
+				cislo = int(args[0])
+				cilova_ciselna_soustava = int(args[1])
+				if cilova_ciselna_soustava < 2 or cilova_ciselna_soustava > 32:
+					print('Parametr <cilova_ciselna_soustava>(2..32) je mimo povoleny rozsah 3..32.')
+				return cislo, cilova_ciselna_soustava
+			except:
+				print('Nespravne zadane parametry: <cislo> <cilova_ciselna_soustava>(2..32)')
+
+	# nacitani parametru <cislo> od uzivatele
+	while True:
+		try:
+			cislo = int(input('Zadejte kladne cele cislo: '))
+			break
+		except:
+			print('Spatny vstup. Zadejte pouze kladne cele cislo.')
+
+	# nacitani parametru <cilova_ciselna_soustava>
+	while True:
+		try:
+			cilova_ciselna_soustava = int(input('Zadejte cilovou ciselnou soustavu (2..32): '))
+			if cilova_ciselna_soustava <= 32 and cilova_ciselna_soustava >= 2:
+				break
+		except:
+			print('Spatny vstup. Zadejte cilovou ciselnou soustavu jako cele kladne cislo.')
+		else:
+			print('Spatny vstup. Zadejte cilovou ciselnou soustavu v rozsahu intervalu (2..32)')
+
+	return cislo, cilova_ciselna_soustava
+
+def preved(cislo: int, cilova_ciselna_soustava: int) -> str:
+	'''
+	Metoda preved prevede <cislo> z desitkove soustavy do ciselne soustavy se zakladem <cilova_ciselna_soustava>
+	'''
+
+	# cislo se bude nejprve ukladat do pole znaku
+	prevedene = []
+
+	prevedene.append(DIGITS[cislo % cilova_ciselna_soustava])
+	cislo //= cilova_ciselna_soustava
+
+	while cislo:
+		prevedene.append(DIGITS[cislo % cilova_ciselna_soustava])
+		cislo //= cilova_ciselna_soustava
+
+	# pole znaku se prevede na string ve spravnem (opacnem nez bylo vkladane do pole) poradi
+	vysledek = ''
+	for s in prevedene[::-1]:
+		vysledek += s
+	return vysledek
+
+def test():
+	assert preved(98,17)=='5D'
+	assert preved(77,3)=='2212'
+
+if __name__=='__main__':
+	main()
